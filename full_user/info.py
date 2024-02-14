a8711fa6c96b5b3885429c03dcd071c4bd886faa
HarshRaj709 <harshsahu709@gmail.com>
2024-02-14 13:13:49 +0530

    final commit

--- full_user/info.py
+++ full_user/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'harshrajdjango709@gmail.com'
-EMAIL_HOST_PASSWORD = 'gxvw zevx dtmv lphj'
+EMAIL_HOST_USER = 'Enter your Email address from which you want to send your Email'
+EMAIL_HOST_PASSWORD = 'Here your Password'
 EMAIL_PORT = 587
